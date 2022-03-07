922f2d6b33e1045d00676c3244c4e384472368d8
Nashara J J <54040002+Nashara-Marrow@users.noreply.github.com>
2022-03-07 18:00:22 +0000

    Completed Lab 4 !

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ In this lab we had to create the cosine , tangent and sine curve on an axis. We also had to create functions that produced each curve.
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 5 
 
-Grace days remaining: < # >/5
+Grace days remaining: 5 /5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ I  completed everything.
 
 ## REFERENCES:
- < List any outside resources used >
+ I used google to make sure my tangent curve looked the same as an actual tangent curve.
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+  It was kind of difficult. 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import turtle
 
 ########### Your Code here ##############
@@ -6,15 +7,69 @@ import turtle
 # then you do not fully understand functions
 # and should review how they work or ask for help
 
+#fred.goto(50,60)
+#wn.exitonclick()
+
+##fred.goto(43,96)
+##fred.goto(98,-76)
+#fred.goto(-85,62)
+#fred.goto(105,63)
+
+#for plot in range ():
+  #drawSineCurve=(-360,360)
 
 
 
 
+
+
+
+def drawSineCurve(fred=None):
+  fred.goto(-360,0)
+  for i in range(-360,361):#this is the X value = i
+  #second value is always exclusive ex if you goto 1-10 it is really 1-9
+    generating_values = math.sin(math.radians(i))#this is where Y values are generated 
+    fred.down()
+    fred.goto(i,generating_values)
+  fred.up()
+
+def setupWindow(wn=None):
+  wn.reset()
+  wn.setworldcoordinates (-360 , -1 , 360 , 1)
+
+def setupAxis(dart=None):
+  dart.up()
+  dart.goto(-360,0)
+  dart.down()
+  dart.goto(360,0)
+  dart.up()
+  dart.goto(0,1)
+  dart.down()  
+  dart.goto(0,-1)
+  dart.up()
+
+
+  
+def drawCosineCurve(dart):
+  dart.goto(-360,1)
+  for i in range (-360,361):
+   generating_values = math.cos(math.radians(i))
+   dart.down()
+   dart.goto(i,generating_values)
+  dart.up()
+
+def drawTangentCurve(dart):
+  dart.goto(-360,0)
+  for i in range (-360,361):
+    generating_values = math.tan(math.radians(i))
+    dart.down()
+    dart.goto(i,generating_values )
+  dart.up()
 
 
 
 ##########  Do Not Alter Any Code Past Here ########
-def main():
+def main(): #this is how you create a function
     #Part A
     wn = turtle.Screen()
     wn.tracer(5)
